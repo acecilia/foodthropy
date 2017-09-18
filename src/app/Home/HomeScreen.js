c6a28a06fbd420497bddec850d61ae630a1aa567
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-09-19 03:39:18 +0800

    Implementation of Locations partially finished

--- src/app/Home/HomeScreen.js
+++ src/app/Home/HomeScreen.js
@@ -1,48 +1,145 @@
+import React, { PureComponent } from "react"
+import { View, Text, FlatList, ActivityIndicator } from "react-native"
+import { List, ListItem, SearchBar } from "react-native-elements"
 
-import React, { Component } from 'react';
-import { ActivityIndicator, ListView, Text, View } from 'react-native';
-
-export class HomeScreen extends React.Component {
+export class HomeScreen extends React.PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
+
+    this.constant = {
+    	baseUrl: "http://localhost:8080",
+    	firstPage: "/locations"
+    }
+
     this.state = {
-      isLoading: true
+      data: [],
+      refreshing: false
+    }
+
+    this.logic = {
+    	error: null,
+    	loading: false,
+    	next: this.constant.firstPage
     }
   }
 
-  componentDidMount() {
-    return fetch('http://localhost:8080/locations')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.setState({
-          isLoading: false,
-          dataSource: ds.cloneWithRows(responseJson),
-        }, function() {
-          // do something with new state
-        });
+  componentDidMount() { }
+
+  makeRemoteRequest = () => {
+  	// Preconditions
+  	if (this.logic.loading === true) { return }
+  	if (this.state.refreshing === true) { this.logic.next = this.constant.firstPage }
+  	if (this.logic.next === null) { return }
+
+  	// Start request
+  	this.logic.loading = true
+    url = this.constant.baseUrl + this.logic.next
+    console.log("Request started: " + url)
+
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+    	console.log("Request arrived: " + url)
+    	
+    	this.logic.next = res.meta.paginator.links.next
+    	this.logic.error = res.error || null
+    	this.logic.loading = false
+
+      this.setState({
+        data: res.meta.paginator.currentPage === 1 ? res.data : [...this.state.data, ...res.data],
+        refreshing: false
       })
-      .catch((error) => {
-        console.error(error);
-      });
+    })
+    .catch(error => {
+    	this.logic.error = error
+    	this.logic.loading = false
+    	this.state.refreshing = false
+
+      this.forceUpdate()
+    })
+  }
+
+  handleRefresh = () => {    
+    this.setState({ refreshing: true }, () => {
+    	this.makeRemoteRequest()
+    })
+  }
+
+  handleLoadMore = () => {    
+    this.makeRemoteRequest()
+  }
+
+  renderSeparator = () => {
+    return (
+	    <View style = {{ 
+	    	height: 1, 
+	    	width: "86%", 
+	    	backgroundColor: "#CED0CE", marginLeft: "14%" 
+	    }}/>
+    )
+  }
+
+  renderSearchBar = () => {
+    return <SearchBar placeholder = "Type Here..." lightTheme round />
+  }
+
+  renderLoading = () => {
+    return (
+	    <View style = {{ paddingVertical: 20 }}>
+	        <ActivityIndicator animating size = "large"/>
+	    </View>
+    )
+  }
+
+  renderEmpty = () => {
+  	return <Text>"Empty"</Text>
+  }
+
+  renderError = () => {
+  		return <Text>Error</Text>
+  }
+
+  renderFooter = () => {
+    if (this.logic.next === null) {
+    	if (this.state.data.length === 0) {
+      	return this.renderEmpty()
+    	} else {
+    		return null
+    	}
+    }
+
+    if (this.logic.error && this.state.data.length === 0) {
+  		return this.renderError()
+  	}
+
+    return this.renderLoading()
   }
 
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
+  	console.log("Render")
 
     return (
-      <View style={{flex: 1, paddingTop: 20}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData.id}, {rowData.name}</Text>}
-        />
-      </View>
-    );
+	    <FlatList style={{backgroundColor: 'white'}}
+	      data = { this.state.data }
+	      //extraData = { this.state }
+	      renderItem = {({ item }) => (
+	        <ListItem
+	          //roundAvatar
+	          title = { item.name }
+	          subtitle = { item.name }
+	          //avatar = {{ uri: item.picture.thumbnail }}
+	          containerStyle = {{ borderBottomWidth: 0 }}
+	        />
+	      )}
+	      keyExtractor = { item => item.name }
+	      ItemSeparatorComponent = { this.renderSeparator }
+	      ListHeaderComponent = { this.renderSearchBar }
+	      ListFooterComponent = { this.renderFooter }
+	      onRefresh = { this.handleRefresh }
+	      refreshing = { this.state.refreshing }
+	      onEndReached = { this.handleLoadMore }
+	      onEndReachedThreshold = { 0 } // Usually usuer will use the search bar, not scroll. We just need some data to fill the view
+	    />
+    )
   }
 }
